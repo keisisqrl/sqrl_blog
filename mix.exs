612f4032686139b37d79b63c029ef109475f5287
Amy Wilhelm <keisisqrl@thexyin.sexy>
2017-04-09 07:40:55 -0700

    Added deps for coherence

--- mix.exs
+++ mix.exs
@@ -38,7 +38,8 @@ defmodule SqrlBlog.Mixfile do
      {:phoenix_live_reload, "~> 1.0", only: :dev},
      {:gettext, "~> 0.11"},
      {:cowboy, "~> 1.0"},
-     {:earmark, "~> 1.2"}
+     {:earmark, "~> 1.2"},
+     {:coherence, "~> 0.3"}
    ]
   end
 
