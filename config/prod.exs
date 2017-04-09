781eff30bd06cd6d4498ad2dbe3b334a7874b612
Amy Wilhelm <keisisqrl@thexyin.sexy>
2017-04-09 10:43:52 -0700

    Move MIX_ENV, HTTP_HOST to env vars

--- config/prod.exs
+++ config/prod.exs
@@ -13,7 +13,7 @@ use Mix.Config
 # which you typically run after static files are built.
 config :sqrl_blog, SqrlBlog.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [scheme: "https", host: "blog.squirrelsoft.org",
+  url: [scheme: "https", host: System.get_env("HTTP_HOST"),
     port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/manifest.json",
